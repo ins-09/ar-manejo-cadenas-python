9b101e0ed1ac2c4c86d53db4a18254ec0e1a306d
ins09 <alexromanr469@gmail.com>
2025-10-22 02:48:20 +0200

    Implementado los tres sistemas de formateo de cadenas

--- ejercicios/ejercicio2.py
+++ ejercicios/ejercicio2.py
@@ -16,9 +16,34 @@ Uso de f-string
 Deben existir tres print
 """
 
+######################
+# DECLARACIÓN E INICIALIZACIÓN DE VARIABLES
+######################
 nombre_persona = "Juan"
 nota1 = 8
 nota2 = 4
 nota3 = 7
+nota_promedio = (nota1+nota2+nota3)/3
 
+# VARIABLES CONSTANTES
+COLOR_ROJO = "\033[31m"
+COLOR_VERDE = "\033[32m"
+COLOR_CYAN = "\u001b[36m"
+
+######################
+# FORMATEO DE CADENAS
+######################
+# Para limitar la cantidad de decimales a 1 utilizaré el siguiente formateo: '.nf' donde n corresponde a 1
+
+# Uso de %
+cadena = COLOR_ROJO+"Hola, mi nombre es %s y tengo un promedio de %.1f en mi libreta" % (nombre_persona, nota_promedio)
+print(cadena)
+
+# Uso de str.format()
+cadena = COLOR_VERDE+"Hola, mi nombre es {} y tengo un promedio de {:.1f} en mi libreta".format(nombre_persona, nota_promedio)
+print(cadena)
+
+# Uso de f-string
+cadena = COLOR_CYAN+f"Hola, mi nombre es {nombre_persona} y tengo un promedio de {nota_promedio:.1f} en mi libreta"
+print(cadena)
 
